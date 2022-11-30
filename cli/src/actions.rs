aa32bce26007c66851db8ca5945ed9f84dced9bd
Daniel Wilk <wilkdaniel03@gmail.com>
2022-11-30 22:34:35 +0100

    [cli] Create actions's from_str constructor

--- cli/src/actions.rs
+++ cli/src/actions.rs
@@ -1,3 +1,14 @@
 pub enum Actions {
     Deformat
 }
+
+impl Actions {
+    pub fn from_str(s: &str) -> Actions {
+        let action = match s {
+            "deformat" => Actions::Deformat,
+            _ => unimplemented!()
+        };
+
+        action
+    }
+}
